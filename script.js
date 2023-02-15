f800cc8fde64b9e599849b581780198fe30ffc3e
Jordan Rammelsberg <jordan.rammelsberg@gmail.com>
2023-02-15 12:18:23 -0600

    grabbed elements from the html and made them into consts. Added function that listens for clicks and displays them in the console. Next will work on dipsplaying values from buttons on the calc screen.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+
+// grab components from html
+const screenCurrent = document.querySelector('.screen-current');
+const screenPrevious = document.querySelector('.screen-previous');
+const numberBtns = document.querySelectorAll('[data-number]');
+const operatorBtns = document.querySelectorAll('[data-operator]');
+const clearBtn = document.getElementById('btnClear');
+const deleteBtn = document.getElementById('btnDelete');
+const pointBtn = document.getElementById('period');
+const equalsBtn = document.getElementById('equals');
+
+
+numberBtns.forEach((button) => button.addEventListener('click', () => console.log(button.textContent))
+)
+
+operatorBtns.forEach((button) => button.addEventListener('click', () => console.log(button.textContent))
+)
+
 const add = (a, b) => {
     return a + b;
 };
@@ -21,15 +40,17 @@ const operate = (operator, a, b) => {
     a = Number(a)
     b = Number(b)
     switch (operator) {
-        case 'add':
+        case '+':
             return add(a, b);
-        case 'subtract':
+        case '-':
             return subtract(a, b);
-        case 'multiply':
+        case '×':
             return multiply(a, b);
-        case 'divide':
+        case '÷':
             return divide(a, b);
         default:
             return null
     };
 };
+
+
