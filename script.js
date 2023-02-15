97f0fb328ad5534e994e820efc12e28a53b2790a
Jordan Rammelsberg <jordan.rammelsberg@gmail.com>
2023-02-15 11:09:00 -0600

    added basic functions for basic operations. Added a while loop incase user tries to divide by zero.

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ const subtract = (a, b) => {
 };
 
 const multiply = (a, b) => {
-
+    return a * b;
 };
 
 const divide = (a, b) => {
-    return a
-}
+    while (b === 0) {
+        return "Ah Ah Ah! No divide by zero!";
+    };
+    return a / b;
+};
+
+
