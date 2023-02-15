3de479454684f8c7e22e00f4c5df1d9c4d2041f9
Jordan Rammelsberg <jordan.rammelsberg@gmail.com>
2023-02-15 16:35:00 -0600

    built out functions for calculator. Can perform basic operations. Clear button resets entire calculator. Delete button deletes one operand at a time and doesn't let user delete if there is a '0' showing. If there is one digit left, the delete button resets display to '0'.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let previousOperand = '';
+let nextOperand = '';
+let currentOperation = null;
+let shouldResetScreen = false;
 
 
 // grab components from html
@@ -10,33 +14,101 @@ const deleteBtn = document.getElementById('btnDelete');
 const pointBtn = document.getElementById('period');
 const equalsBtn = document.getElementById('equals');
 
+// functions for non number and operation buttons
+equalsBtn.addEventListener('click', calculate)
+clearBtn.addEventListener('click', clear);
+pointBtn.addEventListener('click', handlePoint);
+deleteBtn.addEventListener('click', handleDelete);
 
-numberBtns.forEach((button) => button.addEventListener('click', () => console.log(button.textContent))
-)
 
-operatorBtns.forEach((button) => button.addEventListener('click', () => console.log(button.textContent))
-)
+numberBtns.forEach((button) => 
+    button.addEventListener('click', () => displayNumber(button.textContent))
+);
 
-const add = (a, b) => {
+operatorBtns.forEach((button) => 
+    button.addEventListener('click', () => setOperation(button.textContent))
+);
+
+function displayNumber(number) {
+    if (screenCurrent.textContent === '0' || shouldResetScreen)
+        resetScreen();
+    screenCurrent.textContent += number;
+};
+
+function setOperation(operator) {
+    if (currentOperation !== null) calculate();
+    previousOperand = screenCurrent.textContent
+    currentOperation = operator
+    screenPrevious.textContent = `${previousOperand} ${currentOperation}`
+    shouldResetScreen = true
+}
+
+function resetScreen() {
+    screenCurrent.textContent = '';
+    shouldResetScreen = false;
+};
+
+function clear() {
+    screenCurrent.textContent = '0';
+    screenPrevious.textContent = '';
+    previousOperand = '';
+    nextOperand = '';
+    currentOperation = null;
+};
+
+function calculate() {
+    if (currentOperation === null || shouldResetScreen) return;
+    if (currentOperation === '÷' && screenCurrent.textContent === '0') {
+        alert("Ah Ah Ah! No no divide by zero")
+        return
+    }
+
+    nextOperand = screenCurrent.textContent;
+    screenCurrent.textContent = round(operate(currentOperation, previousOperand, nextOperand));
+    screenPrevious.textContent = `${previousOperand} ${currentOperation} ${nextOperand} =`
+    currentOperation = null
+}
+
+function handlePoint() {
+    if (shouldResetScreen) resetScreen();
+    if (screenCurrent.textContent === '')
+        screenCurrent.textContent = '0';
+    if (screenCurrent.textContent.includes('.')) return
+        screenCurrent.textContent += '.'
+}
+
+function handleDelete() {
+    if (screenCurrent.textContent === '0') return;
+    if (screenCurrent.textContent.length === 1) {
+        return screenCurrent.textContent = '0'
+    }
+    screenCurrent.textContent = screenCurrent.textContent.toString().slice(0, -1)
+    
+}
+
+function round(number) {
+    return Math.round(number * 1000000) / 1000000;
+}
+
+
+// operation functions
+function add(a, b) {
     return a + b;
 };
 
-const subtract = (a, b) => {
+function subtract(a, b) {
     return a - b;
 };
 
-const multiply = (a, b) => {
+function multiply(a, b) {
     return a * b;
 };
 
-const divide = (a, b) => {
-    while (b === 0) {
-        return "Ah Ah Ah! No divide by zero!";
-    };
+function divide(a, b) {
     return a / b;
 };
 
-const operate = (operator, a, b) => {
+function operate(operator, a, b) {
     a = Number(a)
     b = Number(b)
     switch (operator) {
@@ -47,7 +119,8 @@ const operate = (operator, a, b) => {
         case '×':
             return multiply(a, b);
         case '÷':
-            return divide(a, b);
+            if (b === 0) return null;
+            else return divide(a, b);
         default:
             return null
     };
